55c8ed48e0f75df5f2ad3bc085e895ff2ac718f7
Jose Garay <garayjrjose@gmail.com>
2019-02-25 23:03:24 -0800

    Finished layout for wares and functionality for the submit button is being tested

---
+++ Project/script.js
@@ -0,0 +1,19 @@
+//This was in the Bootstrap documentation, I did not write this.
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        alert("Thanks for subscribing to our newsletter!");
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
