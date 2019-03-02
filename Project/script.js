613617b3fc502e8ae8fb1429928924a3b9e7c23e
Jose Garay <garayj@oregonstate.edu>
2019-03-02 01:31:19 -0800

    Potions and Weapons art done. Functionality for Modals implemented

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,30 @@
+//Used W3 schools to help me create modals for the wares.
+
+var modal = document.getElementById('Modal');
+let imgs = document.querySelectorAll(".card img");
+var modalImg = document.getElementById("content");
+var captionText = document.getElementById("caption");
+
+
+imgs.forEach( function(element) {
+  element.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  })
+});
+
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click",  function() {
+  modal.style.display = "none";
+});
+
 
 //This was in the Bootstrap documentation, I did not write this.
 (function() {
