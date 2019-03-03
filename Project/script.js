323e7ffc7bf3a9069e6eb4e0647b6b358e55790e
Jose Garay <garayj@oregonstate.edu>
2019-03-03 14:09:09 -0800

    Finished Project

--- Project/script.js
+++ Project/script.js
@@ -1,11 +1,21 @@
-//Used W3 schools to help me create modals for the wares.
 
+//Let the user open the map in a printable view.
+let downloadButton = document.getElementById("download");
+// If there is an element on the page to download, open the Salvisor Map.
+if(downloadButton){
+  downloadButton.addEventListener("click", function(){
+    window.open('img/Salvisor_Map.pdf');
+  });
+}
+
+
+//Used W3 schools to help me create modals for the wares.
 var modal = document.getElementById('Modal');
 let imgs = document.querySelectorAll(".card img");
 var modalImg = document.getElementById("content");
 var captionText = document.getElementById("caption");
 
-
+// For each element within all of the images, add an event listener that fills in the modal.
 imgs.forEach( function(element) {
   element.addEventListener("click", function(){
     modal.style.display = "block";
@@ -14,16 +24,17 @@ imgs.forEach( function(element) {
   })
 });
 
-
-
-
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
+//If the span is there, add the event listener to have the modal go away.
+if(span[0]){
+  span = span[0];
 // When the user clicks on <span> (x), close the modal
-span.addEventListener("click",  function() {
-  modal.style.display = "none";
-});
+  span.addEventListener("click",  function() {
+    modal.style.display = "none";
+  });
+}
 
 
 //This was in the Bootstrap documentation, I did not write this.
@@ -39,7 +50,7 @@ span.addEventListener("click",  function() {
           event.preventDefault();
           event.stopPropagation();
         }
-        alert("Thanks for subscribing to our newsletter!");
+        alert("Thanks For Subscribing!");
         form.classList.add('was-validated');
       }, false);
     });
